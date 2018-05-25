4b4c636a497b0628362af101888be5bcdbdc4e1b
Elle Faraday <faraday.elle@gmail.com>
2018-05-25 15:21:23 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,24 +1,24 @@
 class SchoolClassesController < ApplicationController
     def new
-      @school_class = Student.new
+      @school_class = SchoolClass.new
     end
 
     def show
-      @school_class = Student.find(params[:id])
+      @school_class = SchoolClass.find(params[:id])
     end
 
     def edit
-      @school_class = Student.find(params[:id])
+      @school_class = SchoolClass.find(params[:id])
     end
 
     def create
-      @school_class = Student.new(school_class_params)
+      @school_class = SchoolClass.new(school_class_params)
       @school_class.save
-      redirect_to student_path(@school_class)
+      redirect_to school_class_path(@school_class)
     end
 
     def update
-      @school_class = Student.find(params[:id])
+      @school_class = SchoolClass.find(params[:id])
       @school_class.update(school_class_params)
       @school_class.save
       redirect_to student_path(@school_class)
