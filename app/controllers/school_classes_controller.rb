f10a693a7b48047696faba4f757fa45284ab2ffa
Elle Faraday <faraday.elle@gmail.com>
2018-05-25 15:20:21 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,16 +1,31 @@
 class SchoolClassesController < ApplicationController
-  def new
-  end
+    def new
+      @school_class = Student.new
+    end
 
-  def show
-  end
+    def show
+      @school_class = Student.find(params[:id])
+    end
 
-  def edit
-  end
+    def edit
+      @school_class = Student.find(params[:id])
+    end
 
-  def create
-  end
+    def create
+      @school_class = Student.new(student_params)
+      @school_class.save
+      redirect_to student_path(@school_class)
+    end
 
-  def update
+    def update
+      @school_class = Student.find(params[:id])
+      @school_class.update(student_params)
+      @school_class.save
+      redirect_to student_path(@school_class)
+    end
+
+    private
+    def student_params
+      params.require(:school_class).permit(:first_name, :last_name)
+    end
   end
-end
