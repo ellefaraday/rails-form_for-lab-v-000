ecce7c3cc94715ae22cc703ea00dbd8c849d3aea
Elle Faraday <faraday.elle@gmail.com>
2018-05-25 15:20:45 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,14 +12,14 @@ class SchoolClassesController < ApplicationController
     end
 
     def create
-      @school_class = Student.new(student_params)
+      @school_class = Student.new(school_class_params)
       @school_class.save
       redirect_to student_path(@school_class)
     end
 
     def update
       @school_class = Student.find(params[:id])
-      @school_class.update(student_params)
+      @school_class.update(school_class_params)
       @school_class.save
       redirect_to student_path(@school_class)
     end
